c5b0abf8ca68818d2345470dbd3d7c47e7b64007
topclassprogrammer <topclassprogrammer@gmail.com>
2026-02-20 21:22:02 +0300

    edited main.py

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ if __name__ == "__main__":
     create_tables()
 
     parser = Parser()
-    parser.add_argument("period")
+    parser.add_argument("--period", "-p")
+    parser.add_argument("--currency", "-c")
     args = parser.parse_arguments()
 
     load_dotenv()
-    exchange = Exchange("https://v6.exchangerate-api.com/v6/", os.getenv("API_KEY"), "USD")
+    exchange = Exchange("https://v6.exchangerate-api.com/v6/", os.getenv("API_KEY"), args.currency)
     exchange_rates = exchange.get_exchange_rates()
     request_obj_id = exchange.save_from_currency()
     while True:
